296a17a65734e097cadbcc3651a6e355169be3e6
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-21 17:30:37 -0300

    adding connection to localstorage

--- script.js
+++ script.js
@@ -7,29 +7,21 @@ const Modal = {
   },
 };
 
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
+  },
+
+  set(transactions) {
+    localStorage.setItem(
+      "dev.finances:transactions",
+      JSON.stringify(transactions)
+    );
+  },
+};
+
 const Transaction = {
-  all: [
-    {
-      description: "Luz",
-      amount: -50000,
-      date: "23/01/2021",
-    },
-    {
-      description: "Website",
-      amount: 500000,
-      date: "23/01/2021",
-    },
-    {
-      description: "Internet",
-      amount: -20000,
-      date: "23/01/2021",
-    },
-    {
-      description: "App",
-      amount: 200000,
-      date: "23/01/2021",
-    },
-  ],
+  all: Storage.get(),
 
   add(transaction) {
     Transaction.all.push(transaction);
@@ -198,6 +190,7 @@ const App = {
   init() {
     Transaction.all.forEach(DOM.addTransaction);
     DOM.updateBalance();
+    Storage.set(Transaction.all);
   },
 
   reload() {
